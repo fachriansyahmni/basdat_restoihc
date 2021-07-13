43adc2bae456a7035f5113f4dec2ac5d75dffc3f
unknown <ferminaufal@gmail.com>
2021-07-13 11:57:09 +0700

    add controller

---
+++ app/Http/Controllers/PesananController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Pesanan;
+
+class PesananController extends Controller
+{
+    //
+}
