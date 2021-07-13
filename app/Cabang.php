9715434d564afef9ce17804a583eb2554d258816
CarakaMuhamadRahman <cmr220821@gmail.com>
2021-07-13 12:00:50 +0700

    Edit

--- app/Cabang.php
+++ app/Cabang.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cabang extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $fillable = [
+        'User', 'Cabang', 'Receipt', 'Menu', 'Pesanan'
+    ];
 }
--- app/Menu.php
+++ app/Menu.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $fillable = [
+        'User', 'Cabang', 'Receipt', 'Menu', 'Pesanan'
+    ];
 }
