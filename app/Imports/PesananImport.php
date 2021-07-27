cd35c1d8035fac245a4c610073e3e1323ce6aeb2
unknown <ferminaufal@gmail.com>
2021-07-27 15:05:29 +0700

    update export orders

--- app/Imports/PesananImport.php
+++ app/Imports/PesananImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Pesanan;
+use App\Receipt;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Support\Facades\DB;
@@ -14,23 +15,59 @@ class PesananImport implements ToModel, WithHeadingRow
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function model(array $row)
+    public function model(array $rows)
     {
+        $data = [];
+        $datas = [];
+        $receiptids = DB::table('receipt')
+            ->select('receiptId')
+            ->where('receiptId', $rows['id_receipt'])
+            ->get();
+
         $idMenu = DB::table('menu')
             ->select('id')
-            ->where('namaMenu', $row['menu_pesanan'])
+            ->where('namaMenu', $rows['menu_pesanan'])
             ->get();
 
         $receiptid = DB::table('receipt')
             ->select('receiptId')
-            ->where('nama_pelanggan', $row['nama_pelanggan'])
+            ->where('nama_pelanggan', $rows['nama_pelanggan'])
             ->get();
 
-        return new Pesanan([
-            'receiptid' => $receiptid[0]->receiptId,
-            'jmlMenu' => $row['jumlah_menu'],
-            'noMeja' => $row['nomor_meja'],
-            'idMenu' => $idMenu[0]->id,
-        ]);
+        if ($receiptids[0]->receiptId == $rows['id_receipt']) {
+
+            $datas[] = array(
+                'receiptid' => $receiptid[0]->receiptId,
+                'jmlMenu' => $rows['jumlah_menu'],
+                'noMeja' => $rows['nomor_meja'],
+                'idMenu' => $idMenu[0]->id,
+                'created_at' => now(),
+                'updated_at' => now()
+            );
+            DB::table('pesanan')->insert($datas);
+        } else {
+            $data[] = array(
+                'receiptId' => $rows['id_receipt'],
+                'idPegawai' => $rows['id_pegawai'],
+                'nama_pelanggan' => $rows['nama_pelanggan'],
+                'totalHarga' => $rows['total_harga'],
+                'jmlBayar' => $rows['jumlah_bayar'],
+                'created_at' => now(),
+                'updated_at' => now()
+            );
+            // }
+            DB::table('receipt')->insert($data);
+
+            $datas[] = array(
+                'receiptid' => $receiptid[0]->receiptId,
+                'jmlMenu' => $rows['jumlah_menu'],
+                'noMeja' => $rows['nomor_meja'],
+                'idMenu' => $idMenu[0]->id,
+                'created_at' => now(),
+                'updated_at' => now()
+            );
+            // }
+            DB::table('pesanan')->insert($datas);
+        }
     }
 }
